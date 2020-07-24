36177c13fd535ad02ca7dca49b153c08516969d8
Anastasia Kubova <v-ankubo@microsoft.com>
2020-07-24 11:56:04 +0300

    Resolve feedback

--- Scripts/sort-schemes.sh
+++ Scripts/sort-schemes.sh
@@ -13,7 +13,7 @@ aliasSpase='<space>'
 # Scheme counter.
 currentSchemeNumber=0;
 
-replace_number() {
+replace_number_or_hide_scheme() {
 
     # Get params and remove aliases from schema name.
     schemeName="${1//${aliasSpase}/ }_^#shared#^_"
@@ -22,8 +22,7 @@ replace_number() {
 
     # Prepare the backup file.
     backupFile="${fileName}.bak"
-
-    if [ $isHidden = "true" ] ; then
+    if $isHidden ; then
 
         # Regex for search place for insert hidden value.
         numberPattern="\(<key>${schemeName}<\/key>[${aliasNewLine}][[:space:]]*<dict>[${aliasNewLine}][[:space:]]*\)\(<key>orderHint\)"
@@ -52,7 +51,7 @@ replace_number() {
     rm ${backupFile}
 
     # Increase number of scheme.
-    if [ $isHidden = "false" ] ; then
+    if ! $isHidden ; then
         currentSchemeNumber=$((currentSchemeNumber+1))
     fi
 }
@@ -82,7 +81,7 @@ sort_schemes_function () {
             schemeName="${scheme// /${aliasSpase}}"
 
             # Call replacing function.
-            replace_number ${schemeName} ${fileName} ${isHidden}
+            replace_number_or_hide_scheme ${schemeName} ${fileName} ${isHidden}
         done
     else
         echo "The file $fileName not exist."
@@ -90,22 +89,22 @@ sort_schemes_function () {
 }
 
 # Sort scheme in the AppCenter.xcworkspace project.
-sort_schemes_function "AppCenter.xcworkspace" "false"
+sort_schemes_function "AppCenter.xcworkspace" false
 
 # Sort schemes in modules projects.
-sort_schemes_function "AppCenter/AppCenter.xcodeproj" "false"
-sort_schemes_function "AppCenterAnalytics/AppCenterAnalytics.xcodeproj" "false"
-sort_schemes_function "AppCenterCrashes/AppCenterCrashes.xcodeproj" "false"
-sort_schemes_function "AppCenterDistribute/AppCenterDistribute.xcodeproj" "false"
+sort_schemes_function "AppCenter/AppCenter.xcodeproj" false
+sort_schemes_function "AppCenterAnalytics/AppCenterAnalytics.xcodeproj" false
+sort_schemes_function "AppCenterCrashes/AppCenterCrashes.xcodeproj" false
+sort_schemes_function "AppCenterDistribute/AppCenterDistribute.xcodeproj" false
 sort_schemes_function "AppCenterPush/AppCenterPush.xcodeproj" "false"
 
 # Sort schemes in apps projects.
-sort_schemes_function "Sasquatch/Sasquatch.xcodeproj" "false"
-sort_schemes_function "SasquatchMac/SasquatchMac.xcodeproj" "false"
-sort_schemes_function "SasquatchTV/SasquatchTV.xcodeproj" "false"
+sort_schemes_function "Sasquatch/Sasquatch.xcodeproj" false
+sort_schemes_function "SasquatchMac/SasquatchMac.xcodeproj" false
+sort_schemes_function "SasquatchTV/SasquatchTV.xcodeproj" false
 
 # Sort other schemes.
-sort_schemes_function "CrashLib/CrashLib.xcodeproj" "true"
-sort_schemes_function "Vendor/PLCrashReporter/CrashReporter.xcodeproj" "true"
-sort_schemes_function "Vendor/OCMock/Source/OCMock.xcodeproj" "true"
-sort_schemes_function "Vendor/OCHamcrest/Source/OCHamcrest.xcodeproj" "true"
+sort_schemes_function "CrashLib/CrashLib.xcodeproj" false
+sort_schemes_function "Vendor/PLCrashReporter/CrashReporter.xcodeproj" false
+sort_schemes_function "Vendor/OCMock/Source/OCMock.xcodeproj" true
+sort_schemes_function "Vendor/OCHamcrest/Source/OCHamcrest.xcodeproj" true
